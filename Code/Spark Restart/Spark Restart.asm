2debcc3d2807a4c20351363fa940aeb826bc07dc
testyourmine <25854988+testyourmine@users.noreply.github.com>
2023-07-10 12:41:10 -0500

    Added Super Missile fix and Shinespark Restart disassemblies

---
+++ Code/Spark Restart/Spark Restart.asm
@@ -0,0 +1,59 @@
+lorom
+
+org $948884: JSR $B1F0
+
+org $94B1F0:
+          CODE_94B1F0: LDA.W $0A1C      
+                       CMP.W #$00C9     
+                       BEQ CODE_94B201  
+                       CMP.W #$00CA     
+                       BEQ CODE_94B24C  
+                       LDA.W #$0001     
+                       RTS              
+                                        
+          CODE_94B201: LDA.B $8B        
+                       BIT.W $09B0      
+                       BNE CODE_94B20C  
+                       LDA.W #$0001     
+                       RTS              
+                                        
+                                        
+          CODE_94B20C: LDA.W #$0009     
+                       STA.W $0A28      
+                       DEC A            
+                       STA.W $0A1E      
+                       LDA.W #$0001     
+                       STA.W $0A1F      
+                                        
+          CODE_94B21C: LDA.W #$0009     
+                       STA.W $0B42      
+                       LDA.W #$FFFE     
+                       STA.B $12        
+                       STZ.B $14        
+                       JSL.L CODE_949763
+                       LDA.W #$0001     
+                       STA.W $0A6E      
+                       STA.W $0B3C      
+                       LDA.W #$0402     
+                       STA.W $0B3E      
+                       STZ.W $0B36      
+                       LDA.W #$A337     
+                       STA.W $0A58      
+                       LDA.W #$E913     
+                       STA.W $0A60      
+                       RTS              
+                                        
+          CODE_94B24C: LDA.B $8B        
+                       BIT.W $09AE      
+                       BNE CODE_94B257  
+                       LDA.W #$0001     
+                       RTS              
+                                        
+                                        
+          CODE_94B257: LDA.W #$000A     
+                       STA.W $0A28      
+                       LDA.W #$0004     
+                       STA.W $0A1E      
+                       LDA.W #$0001     
+                       STA.W $0A1F      
+                       JMP.W CODE_94B21C
---
+++ Code/Super Missile Fix/Super Missile Fix.asm
@@ -0,0 +1,112 @@
+lorom
+
+org $90AC60: BEQ $00
+
+org $90AE19: BRA $02
+
+org $90AFF1: BRA $29
+
+org $90B005:
+                       JSR.W $B329       
+                       LDA.W $0C04,X           
+                       AND.W #$000F            
+                       ASL A                   
+                       TAX                     
+                       JSR.W ($B033,X)
+                       JSR.W $B16A       
+                       BCC Return              
+                       JSL.L $ADB7       
+                                               
+               Return: RTS                     
+
+org $90B2FE
+                       BNE CODE_90B310  
+                       INC A            
+                       STA.W $0C7C,X    
+                       LDA.W #$0100     
+                       STA.B $16        
+                       STX.B $12        
+                       JSR.W $B1F3
+                       BRA CODE_90B313  
+                                        
+          CODE_90B310: JSR.W $B329
+                                        
+          CODE_90B313: PLY              
+                       PLX              
+                       PLP              
+                       RTS              
+
+org $90B362: RTS
+
+org $90B372:
+                       BNE return       
+                       LDA.W $0BF0,X    
+                       BPL CODE_90B37D  
+                       EOR.W #$FFFF     
+                       INC A            
+                                        
+          CODE_90B37D: AND.W #$FF00     
+                       CMP.W #$0B00     
+                       BMI return       
+                       XBA              
+                       SEC              
+                       SBC.W #$000A     
+                       STA.B $12        
+                       LDA.W $0B78,X    
+                       PHA              
+                       BIT.W $0BF0,X    
+                       BMI CODE_90B39A  
+                       SEC              
+                       SBC.B $12        
+                       BRA CODE_90B39D  
+                                        
+                                        
+          CODE_90B39A: CLC              
+                       ADC.B $12        
+                                        
+          CODE_90B39D: STA.W $0B78,X    
+                       JSL.L $94A4D9
+                       LDA.W $0C18,X    
+                       AND.W #$0F00     
+                       CMP.W #$0800     
+                       PLA              
+                       BCS return       
+                       STA.W $0B78,X    
+                                        
+               return: RTS              
+
+org $90B412:
+                       BNE return       
+                       LDA.W $0BDC,X    
+                       BPL CODE_90B41D  
+                       EOR.W #$FFFF     
+                       INC A            
+                                        
+          CODE_90B41D: AND.W #$FF00     
+                       CMP.W #$0B00     
+                       BMI return       
+                       XBA              
+                       SEC              
+                       SBC.W #$000A     
+                       STA.B $12        
+                       LDA.W $0B64,X    
+                       PHA              
+                       BIT.W $0BDC,X    
+                       BMI CODE_90B43A  
+                       SEC              
+                       SBC.B $12        
+                       BRA CODE_90B43D  
+                                        
+          CODE_90B43A: CLC              
+                       ADC.B $12        
+                                        
+          CODE_90B43D: STA.W $0B64,X    
+                       JSL.L $94A46F
+                       LDA.W $0C18,X    
+                       AND.W #$0F00     
+                       CMP.W #$0800     
+                       PLA              
+                       BCS return       
+                       STA.W $0B64,X    
+                                        
+               return: RTS              
